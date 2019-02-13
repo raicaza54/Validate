6dba023003debfc15b0961823765271c50dcdfe8
kevin <kevin@dell>
2019-02-13 00:02:04 -0500

    Interfaz de inicio he integracion con knockout js

--- application/controllers/Auditoria.php
+++ application/controllers/Auditoria.php
@@ -1,5 +1,7 @@
 <?php
 
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,27 +14,22 @@
  * @author Kevin Enriquez
  */
 class Auditoria extends CI_Controller {
-  
+
     /**
      * Variable de carga de vista plantilla
      */
-    private $data = array('body' => '');    
-    
+    private $data = array('body' => '');
+
     public function __construct() {
         parent::__construct();
         if (!$this->ion_auth->logged_in()) {
             redirect('auth/login');
         }
     }
-    
+
     public function index() {
-        $this->data['body'] .= '<br/><br/><a href="' . base_url('tablero/spider/130505') . '">130505</a> - ' .
-                '<a href="' . base_url('tablero/spider/135515') . '">135515</a> - ' .
-                '<a href="' . base_url('tablero/spider/413595') . '">413595</a> - ' .
-                '<a href="' . base_url('tablero/spider/240801') . '">240801</a> - ' .
-                '<a href="' . base_url('tablero/spider/135517') . '">135517</a> - ' .
-                '<a href="' . base_url('tablero/spider/11100501') . '">11100501</a>';
-        $this->load->view("plantilla/plantilla", $this->data);        
+        add_asset("js", "empresas/app.js");
+        $this->load->view("plantilla/plantilla", $this->data);
     }
-    
+
 }
--- application/controllers/Empresas.php
+++ application/controllers/Empresas.php
@@ -11,10 +11,75 @@
  *
  * @author Kevin Enriquez
  */
-class Empresas {
+class Empresas extends CI_Controller {
+  
+    public $response = array(
+        "meta"   => array(
+            "copyright" => "Verify 2019",
+            "authors"   => array(
+                "Kevin Enriquez",
+            )
+        ),
+        "status" => "422",
+        "source" => array(
+            "pointer" => ""
+        ),
+        "title"  => "Invalid Attribute",
+        "detail" => "",
+        "data"   => array()
+    );    
     
     function __construct() {
         parent::__construct();
-        //echo $this->input->is_ajax_request();
+        if (!$this->ion_auth->logged_in()){
+            //redirect('auth/login');
+        }
+        if (!$this->input->is_ajax_request()) {
+            //show_404();
+        }
     }
+    
+    public function datos() {
+        echo "Hola mundo";
+        exit();
+        $response = $this->response;
+        try {
+            if (!is_array($items)) {
+                throw new Exception("No existen datos para mostrar como ejemplo", 204);
+            }
+            $response["data"] = array(
+                'list' => 'Hola mundo'
+            );            
+            throw new Exception("Resultado retornando correctamente", 200);
+        } catch (Exception $ex) {
+            $response["status"] = $exc->getCode();
+            $exception = array(
+                "code"    => $exc->getCode(),
+                "message" => $exc->getMessage(),
+            );
+            if ($exception["code"] === 200) {
+                $response["title"] = "Procedimiento realizado satisfactoriamente";
+                $response["detail"] = (strlen($exception["message"]) && !empty($exception["message"])) ? $exception["message"] : "Petición correcta";
+            } elseif ($exception["code"] === 202) {
+                $response["title"] = "Petición Aceptada pero incompleta";
+                $response["detail"] = (strlen($exception["message"]) && !empty($exception["message"])) ? $exception["message"] : "Petición Aceptada pero incompleta";
+                log_message("error", $exc->getCode().' - '.$exc->getMessage());
+            } elseif ($exception["code"] === 500) {
+                $response["title"] = "Error Interno del Servidor";
+                $response["detail"] = (strlen($exception["message"]) && !empty($exception["message"])) ? $exception["message"] : "Internal Server Error";
+                log_message("error", $exc->getCode().' - '.$exc->getMessage());
+            } else {
+                $response["title"] = "Error Interno del Servidor";
+                $response["detail"] = (strlen($exception["message"]) && !empty($exception["message"])) ? $exception["message"] : "Internal Server Error";
+                log_message("error", $exc->getCode().' - '.$exc->getMessage());
+            }
+        }
+        $jsonResponse = json_encode($response);
+        ob_start();
+        echo $jsonResponse;
+        header('Content-Length: ' . ob_get_length());
+        header('Content-type: application/x-json;charset=UTF-8');
+        ob_flush();        
+    }
+    
 }
