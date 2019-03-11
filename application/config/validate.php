c0e527b7f0ec3353225a13d673f384824b07641b
Ubuntu <ubuntu@ip-172-31-87-115.ec2.internal>
2019-03-11 03:09:28 +0000

    Cambios propios del server

--- application/config/validate.php
+++ application/config/validate.php
@@ -9,7 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Ruta en el servidor donde seran alojados los archivos de cada usuario
 |
 */
-$config['path_file'] = '/home/kevin/archivos';
+$config['path_file'] = '/home/archivos/archivos';
 
 /*
 |--------------------------------------------------------------------------
