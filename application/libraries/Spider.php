866b00d639278980171c9ec120029b1a00ff2e9b
kevin <kevin@dell>
2019-03-25 19:08:18 -0500

    Manipulacion 0.1

--- application/libraries/Spider.php
+++ application/libraries/Spider.php
@@ -26,33 +26,6 @@ class Spider {
     public function procesar($cuenta) {
         try{
         $ejemplo  = $this->data;
-//        $this->CI->load->library('table');
-//        $template = array(
-//            'table_open'         => '<table border="1" cellpadding="4" cellspacing="0">',
-//            'thead_open'         => '<thead>',
-//            'thead_close'        => '</thead>',
-//            'heading_row_start'  => '<tr>',
-//            'heading_row_end'    => '</tr>',
-//            'heading_cell_start' => '<th>',
-//            'heading_cell_end'   => '</th>',
-//            'tbody_open'         => '<tbody>',
-//            'tbody_close'        => '</tbody>',
-//            'row_start'          => '<tr>',
-//            'row_end'            => '</tr>',
-//            'cell_start'         => '<td>',
-//            'cell_end'           => '</td>',
-//            'row_alt_start'      => '<tr>',
-//            'row_alt_end'        => '</tr>',
-//            'cell_alt_start'     => '<td>',
-//            'cell_alt_end'       => '</td>',
-//            'table_close'        => '</table>'
-//            );
-//
-//        $this->CI->table->set_template($template);
-//        $this->CI->table->set_heading('REGISTRO', 'CUENTA', 'CTE', 'FECHA', 'DOC', 'REF', 'NIT', 'DETALLE', 'TIPO', 'VALOR', 'BASE', 'CC', 'TB', 'PL');
-//        $tabla  = $this->CI->table->generate($ejemplo);
-//        $this->CI->table->clear();
-        
         $grupos = array();
         foreach ($ejemplo as $value) {
             $grupos[$value['CTE'] . $value['DOC']][] = array(
@@ -165,6 +138,8 @@ class Spider {
                 }
             }
         }
+        $this->exportEtpCsv($grupos_clasificado['credito'], 'credito');
+        $this->exportEtpCsv($grupos_clasificado['debito'], 'debito');
 //        $tabla_grupos_clasificados = '';
 //        foreach ($grupos_clasificado as $key => $value) {
 //            $tabla_grupos_clasificados .= '############# ' . strtoupper($key) . ' #############';
@@ -298,4 +273,21 @@ class Spider {
         }        
         return $this->datos;
     }    
+    
+    private function exportEtpCsv($data, $archivo) {
+        header("Content-type: application/csv");
+        header("Content-Disposition: attachment; filename=\"test" . ".csv\"");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        $handle = fopen(APPPATH.'logs/'.$archivo.'.csv', 'w');
+        fputcsv($handle, array("grupo", "cuenta", "tipo", "valor"));
+        foreach ($data as $keydat) {
+            foreach ($keydat as $key) {
+                $narray = array($key["grupo"], $key["cuenta"], $key["tipo"], $key["valor"]);
+                fputcsv($handle, $narray);
+            }
+        }
+        fclose($handle);
+    }
+
 }
