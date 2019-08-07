16321e524b533b5b7dc88264f4094df9e59fe104
kevin <kevin@dell>
2019-08-07 00:51:58 -0500

    Pruebas con la funcion levenshtein en MySQL

--- application/models/Listas_model.php
+++ application/models/Listas_model.php
@@ -27,33 +27,30 @@ class Listas_model extends CI_Model {
     }
     
     public function extrac($a, $campo) {
-$query = $this->db->query("
-SELECT
-    *
-FROM
-    (
-    SELECT
-        sl.lista,
-        sl.nombre,
-        sl.aka,
-        sl.identificacion,
-        sl.otros,
-        sl.deleted_at,
-        levenshtein_ratio('$a', sl.$campo) AS ratio
-    FROM
-        sist__listas AS sl
-    WHERE
-        deleted_at = 0
-) AS li
-WHERE
-    li.ratio > 85");
-        
-        //$query = $this->db->query("SELECT * FROM sist__listas WHERE levenshtein_ratio('$a', $campo) > 85 AND deleted_at = 0");
-        $data = $query->result_array();
-        //$this->db->like($campo, $a, 'none');
-        //$this->db->where('deleted_at', 0);
-        //$data = $this->db->get('sist__listas')->result_array();
-        debug_file($data);
+        //$query = $this->db->query("
+        //SELECT
+        //    *
+        //FROM
+        //    (
+        //    SELECT
+        //        sl.lista,
+        //        sl.nombre,
+        //        sl.aka,
+        //        sl.identificacion,
+        //        sl.otros,
+        //        sl.deleted_at,
+        //        levenshtein_ratio('$a', sl.$campo) AS ratio
+        //    FROM
+        //        sist__listas AS sl
+        //    WHERE
+        //        deleted_at = 0
+        //) AS li
+        //WHERE
+        //    li.ratio > 85");
+        //$data = $query->result_array();
+        $this->db->like($campo, $a, 'none');
+        $this->db->where('deleted_at', 0);
+        $data = $this->db->get('sist__listas')->result_array();
         return $data;        
     }
     
