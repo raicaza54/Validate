557de291c2f6d4d47f3bff014d5c8f41825eee0b
kevin <kevin@dell>
2019-09-03 05:47:49 -0500

    Corrección
    Margen superior del logotipo

--- application/views/auth/plantilla.php
+++ application/views/auth/plantilla.php
@@ -27,7 +27,7 @@
         <div class="container">
             <div class="row justify-content-md-center">
                 <?php if(in_array($this->uri->segment(2), ['forgot_password','login','reset_password'])): ?>
-                <div class="col col-lg-5">
+                <div class="col col-lg-5" style="margin-top: 2.5em;">
                     <?=$body?>
                 </div>
                 <?php else: ?>
