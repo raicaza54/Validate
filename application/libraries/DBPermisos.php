7cf2cf3da46ba0f04a9f3069586fa5c83074a731
kevin <kevin@dell>
2020-05-02 12:47:18 -0500

    Permisos Nuevo modulo

--- application/libraries/DBPermisos.php
+++ application/libraries/DBPermisos.php
@@ -156,7 +156,7 @@ class DBPermisos {
             ['fk_permisos' => 403, 'fk_groups' => 2] + $userDate,
             ['fk_permisos' => 405, 'fk_groups' => 2] + $userDate,
             ['fk_permisos' => 410, 'fk_groups' => 2] + $userDate,
-            //['fk_permisos' => 411, 'fk_groups' => 2] + $userDate,
+            ['fk_permisos' => 411, 'fk_groups' => 2] + $userDate,
             ['fk_permisos' => 600, 'fk_groups' => 2] + $userDate,
             ['fk_permisos' => 601, 'fk_groups' => 2] + $userDate,
             ['fk_permisos' => 602, 'fk_groups' => 2] + $userDate,
