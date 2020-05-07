75b8220a39cb0640a4448eb1b64cadf733d21df0
kevin <kevin@dell>
2020-05-07 13:07:08 -0500

    Corrección Condición de Cuenta
    Para archivos Debitos/Creditos la columna valor se crea sumando dos columnas

--- application/models/Archivos_model.php
+++ application/models/Archivos_model.php
@@ -414,7 +414,8 @@ class Archivos_model extends CI_Model {
     
     public function getBases($id_archivo, $column) {
         $this->db->select($column['string']);
-        $this->db->select('(('.$column['array']['valor'].'/'.$column['array']['base'].')*100) AS calculo');
+        $valor = isset($column['array']['valor']) ? $column['array']['valor'] : '('.$column['array']['debe'].' + '.$column['array']['haber'].')';
+        $this->db->select('(('.$valor.'/'.$column['array']['base'].')*100) AS calculo');
         $this->db->where('created_clie', $this->session->userdata('clientes_id'));
         $this->db->where('fk_archivos', $id_archivo);
         $this->db->where('linea', 'f');
