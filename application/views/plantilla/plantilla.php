5e6df85737e7c94be1a67385193fedc9f92b222b
kevin <kevin@dell>
2020-06-29 17:59:43 -0500

    Correccion en opcion de comprar
    0: demo, 1: pago

--- application/views/plantilla/plantilla.php
+++ application/views/plantilla/plantilla.php
@@ -23,7 +23,7 @@
                     Administrador
                 </a>
                 <?php endif; ?>
-                <?php if($this->session->userdata('demostracion') == 1): ?>
+                <?php if($this->session->userdata('demostracion') == 0): ?>
                 <a class="nav-link active nav-icon" href="#" style="font-size: 1rem;" onclick="COMPRAR.methods.comprarModal()">
                     <span style="background-color: #486196; border-radius: 35px; padding: 3px 8px;">
                         <i class="fas fa-comment-dollar"></i> Comprar
